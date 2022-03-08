a4ccc3eaabed42738594fb4e9e76d38b0081078a
jack <8209433+jacktuck@users.noreply.github.com>
2022-03-08 21:34:24 +0000

    cache system calls

--- scripts/status.sh
+++ scripts/status.sh
@@ -4,9 +4,10 @@ export LC_TIME="en_US.UTF-8"
 TIME=$(date +"%H:%M")
 DATE=$(date +"%a %d/%m")
 
-BATTERY_PERCENTAGE=$(pmset -g batt | egrep '([0-9]+\%).*' -o --colour=auto | cut -f1 -d'%')
-BATTERY_STATUS=$(pmset -g batt | grep "'.*'" | sed "s/'//g" | cut -c 18-19)
-BATTERY_REMAINING=$(pmset -g batt | egrep -o '([0-9]+%).*' | cut -d\  -f3)
+BATTERY_CHECK_CMD=$(pmset -g batt)
+BATTERY_PERCENTAGE=$(echo "$BATTERY_CHECK_CMD" | egrep '([0-9]+\%).*' -o --colour=auto | cut -f1 -d'%')
+BATTERY_STATUS=$(echo "$BATTERY_CHECK_CMD" | grep "'.*'" | sed "s/'//g" | cut -c 18-19)
+BATTERY_REMAINING=$(echo "$BATTERY_CHECK_CMD" |  egrep -o '([0-9]+%).*' | cut -d\  -f3)
 
 BATTERY_CHARGING=""
 if [ "$BATTERY_STATUS" == "Ba" ]; then
@@ -22,12 +23,13 @@ WIFI_SSID=$(networksetup -getairportnetwork en0 | cut -c 24-)
 
 DND=$(defaults -currentHost read com.apple.notificationcenterui doNotDisturb)
 
-MEM_INACTIVE=$(memory_pressure | grep "Pages inactive" | grep -o -E '[0-9]+')
-MEM_FREE=$(memory_pressure | grep "Pages free" | grep -o -E '[0-9]+' | awk -v MEM_INACTIVE=$MEM_INACTIVE '{ print $1 + MEM_INACTIVE }')
+MEM_CHECK_CMD=$(memory_pressure)
 
-MEM_TOTAL=$(memory_pressure | grep system | awk -F" " '{print $5}' | grep -o -E '[0-9]+')
+MEM_INACTIVE=$(echo "$MEM_CHECK_CMD" | grep "Pages inactive" | grep -o -E '[0-9]+')
+MEM_FREE=$(echo "$MEM_CHECK_CMD" | grep "Pages free" | grep -o -E '[0-9]+' | awk -v MEM_INACTIVE=$MEM_INACTIVE '{ print $1 + MEM_INACTIVE }')
+MEM_TOTAL=$(echo "$MEM_CHECK_CMD" | grep system | awk -F" " '{print $5}' | grep -o -E '[0-9]+')
+
 MEM_USED=$(echo | awk -v MEM_TOTAL=$MEM_TOTAL -v MEM_FREE=$MEM_FREE '{ print MEM_TOTAL - MEM_FREE }')
-
 MEM_USED_PERCENT=$(echo | awk -v MEM_USED=$MEM_USED -v MEM_TOTAL=$MEM_TOTAL '{ print (MEM_USED / MEM_TOTAL) * 100}')
 
 
